d1e94f4ead92322fce20717fa04ab603a6883c06
Lifesheep1 <165856601+Lifesheep1@users.noreply.github.com>
2024-07-24 11:48:07 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int result1 = 1;
+        int result2 = 0;
+        for(int i=0; i<num_list.length; i++){
+            result1 *= num_list[i];
+            result2 += num_list[i];
+        }
+        if (result1 < result2*result2){
+            answer = 1;
+        }
+        return answer;
+    }
+}
