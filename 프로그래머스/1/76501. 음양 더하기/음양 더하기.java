650ed8e25ebbea3af0a8ef0ea375bc22075b75d2
Lifesheep1 <165856601+Lifesheep1@users.noreply.github.com>
2025-03-18 05:04:18 +0900

    Update 음양 더하기.java

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-        //absolutes의 길이만큼 반복을 먼저하고 그안에서 signs이 붙은 자리는 -를 붙여주고 absolutes를 다 더함
+        //absolutes의 길이만큼 반복을 먼저하고 그안에서 signs이 붙은 자리는 음수로 변환하고 absolutes를 다 더함
         for(int i = 0; i < absolutes.length; i++){
             if(signs[i] == false){
                 absolutes[i] = (absolutes[i] * -1);  
