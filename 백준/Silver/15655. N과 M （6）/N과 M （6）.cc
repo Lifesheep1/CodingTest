f7dfc7be2bea2fc5901ed1275d04ece23ee2d5e5
Lifesheep1 <165856601+Lifesheep1@users.noreply.github.com>
2025-09-08 14:40:04 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[10]; // index: n이 저장되는 값주소 value: n의 값들
+int path[10];
+int used[10];
+
+void func(int now,int start) {
+	//종료
+	if (now == m) {
+		for (int i = 0; i < m; i++)
+		{
+			cout << path[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = start; i < n; i++)
+	{
+		path[now] = arr[i];
+		func(now + 1,i+1);
+	}
+
+
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	func(0,0);
+
+	return 0;
+
+}
